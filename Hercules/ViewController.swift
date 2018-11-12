d547ed3db1739fed45bb7b346256f32ddf1a5a94
Patrick Smith <patrick@burntcaramel.com>
2018-11-12 20:04:25 +1100

    Initial Commit

---
+++ Hercules/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hercules
+//
+//  Created by Patrick Smith on 12/11/18.
+//  Copyright © 2018 Royal Icing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
